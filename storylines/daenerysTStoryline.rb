da425721d881d4d42806426823b71d181230f774
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-06-09 14:45:39 +1000

    Battle sequence enhanced, escape option added. Working.

--- storylines/daenerysTStoryline.rb
+++ storylines/daenerysTStoryline.rb
@@ -12,22 +12,44 @@ module DaenerysTStoryline
         daenerys_targaryen = Players::Player.new("Daenerys Targaryen")
         whitewalker1 = Game_actors::Low_level_game_actor.new("White Walker")
 
-        storyprint("""
-        I'm going to die if I don't get out of here.
-        I need Drogon or Rhaegal!
-        What is this place??
-        This rotting smell is making me sick.
-        And why is it so cold in here!!
-        Thunk thunk.. Thunk thunk
-        """)
+        print_story(
+            """
+            I'm going to die if I don't get out of here.
+            I need Drogon or Rhaegal!
+            What is this place??
+            This rotting smell is making me sick.
+            And why is it so cold in here!!
+            Thunk thunk.. Thunk thunk
+            """
+        )
 
         enemy_encounter_statement(whitewalker1)
         
         user_choice = user_make_choice("Turn Left", "Turn Right")
         if (user_choice == "Turn Left".strip)
+            print_story(
+                """
+                You run down the dimly lit corridor..
+                Bang!!
+                You've ran straight into a wall
+                """
+            )
+            player_battle_enemy_intro(whitewalker1)
             player_battle_enemy(daenerys_targaryen, whitewalker1)
         elsif (user_choice == "Turn Right".strip)
-            player_battle_enemy(daenerys_targaryen, whitewalker1)
+            player_escape_statement(
+                """
+                You run down the dimly lit corridor...
+                The chilling footsteps have turned into a stampede following you...
+                Where is left to run?
+                The shrieking sounds grow louder...
+                ...
+                Over there!!
+                You race towards the giant closing doors...
+                Boom! You scrape through as the doors slam behind you...
+                """
+            )
+            # player_battle_enemy(daenerys_targaryen, whitewalker1)
         else
             user_make_choice("Turn Left", "Turn Right")
         end
--- storylines/story-methods/story-methods.rb
+++ storylines/story-methods/story-methods.rb
@@ -1,6 +1,6 @@
 module Story_methods
 
-    def storyprint(story_string)       
+    def print_story(story_string)       
         story = story_string
         story.each_char do |c|
             print c
@@ -24,23 +24,25 @@ module Story_methods
         users_choice = user_make_choice_prompt.select("", users_options)
     end
 
-    
-
-    def enemy_dead_script()
-        puts "You killed a xyz. My hero"
+    def player_escape_statement(statement)
+        statement.each_char do |c|
+            print c
+            sleep(0.03)
+        end
     end
-    
-    def player_dead_script()
-        puts "You died! Noob!"
+
+    def player_battle_enemy_intro(enemy)
+        statement = """
+                The #{enemy.name} has you trapped!
+                In order to survive you must kill it.
+                """
+        statement.each_char do |c|
+            print c
+            sleep(0.03)
+        end
     end
     
     def player_battle_enemy(player, enemy)
-    puts "You run down the dimly lit corridor.."
-    sleep(2)
-    puts"Bang!!\nYou've ran straight into a wall"
-    sleep(2)
-    puts "The #{enemy.name} has you trapped!\nIn order to survive you must kill it."
-    puts "Press 1. To melee attack"
         until (enemy.health <= 0 || player.health <= 0)
             player_attack_enemy(player, enemy)
         end
@@ -52,7 +54,7 @@ module Story_methods
     end
     
     def player_attack_enemy(player, enemy)
-    "Which attack are you going to do? \n 1. melee attack"
+        puts "Which attack are you going to do? \n1. melee attack"
         if (gets().chomp() == 1.to_s)
             player.player_base_attack(enemy) 
             sleep(3)
@@ -61,6 +63,14 @@ module Story_methods
             puts "You have not inputted an appropriate action\nPress '1' to melee attack"
             gets().chomp()
         end
+    end
+
+    def enemy_dead_script()
+        puts "You killed a xyz. My hero"
+    end
+    
+    def player_dead_script()
+        puts "You died! Noob!"
     end
 
 end
